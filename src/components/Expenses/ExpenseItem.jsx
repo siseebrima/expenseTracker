cdb27bdd208093c48cb3c36b5e54881f367b0cce
Ebrima Sise <siseebrima@gmail.com>
2021-10-07 08:33:59 +0000

    commit from the linux machine

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [newName, setNewName] = useState(title);
+  const handleClick = () => {
+    // console.log("I got clicked");
+    setNewName("American trip!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newName}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={handleClick}>change title</button>
     </Card>
   );
 };
